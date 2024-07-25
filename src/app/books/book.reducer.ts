8c489bfba6a396fa9754eb43f40cb89af05ae0a4
YunusEmreUslu <usluyunusemre00@gmail.com>
2024-07-25 12:52:42 +0300

    created the book reducers and learned about what reducers are and what they do

---
+++ src/app/books/book.reducer.ts
@@ -0,0 +1,20 @@
+import { AddBook, RemoveBook } from './book.actions';
+import { createReducer, on } from "@ngrx/store";
+import { Book } from "../models/book";
+
+export const initialState: ReadonlyArray<Book> = [];
+
+export const BookReducer = createReducer(
+   initialState,
+   on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
+   on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !== bookId))
+);
+
+
+/* Why is a reducer called reducer?
+
+The reducer takes an action and the current state and reduces them to one new state.
+
+action + current state = new state
+
+*/
